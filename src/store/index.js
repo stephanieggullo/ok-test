9574628453e7853ff19b55e70d3262b4939078bf
Stephanie Guevara <stephanie.guevara@kairosds.com>
2021-09-15 17:34:44 +0200

    feat: Adding @reduxjs/toolkit package and app store

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const wizardSlice = createSlice({
+  name: 'wizard',
+  initialState: {
+    step: null,
+    feedback: null,
+  },
+  reducers: {
+    setStep(state, action) {
+      state.step = action.payload;
+    },
+    setFeedback(state, action) {
+      state.feedback = action.payload;
+    },
+  },
+});
+
+const store = configureStore({ reducer: wizardSlice.reducer });
+
+export const wizardActions = wizardSlice.actions;
+export default store;
